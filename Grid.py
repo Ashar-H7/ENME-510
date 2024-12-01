c11c2284da1de86cd53f3f67805daf09cc012e0d
FaresElsaid <90662186+FaresElsaid@users.noreply.github.com>
2024-12-02 01:23:02 +0200

    Update Grid.py

--- Grid.py
+++ Grid.py
@@ -2,33 +2,35 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-
 file_path = 'Data sold to the grid.xlsx'
 df = pd.read_excel(file_path)
 
-
 print(df.head())
 
-# Set the style for the plots
 sns.set(style="whitegrid")
 
+df['year'] = df['year'].ffill()
+
+df['year_month'] = df['year'].astype(int).astype(str) + '-' + df['month_name']
+
+df['year_month'] = pd.to_datetime(df['year_month'], format='%Y-%B')
 
 plt.figure(figsize=(12, 6))
-sns.lineplot(x='month_name', y='Generator Export to Grid (MWh)', data=df, marker='o')
+sns.lineplot(x='year_month', y='Generator Export to Grid (MWh)', data=df, marker='o')
 plt.title("Monthly Generator Export to Grid (MWh)", fontsize=16)
 plt.ylabel("Generator Export to Grid (MWh)", fontsize=14)
 plt.xlabel("Month", fontsize=14)
-plt.xticks(rotation=45)
+plt.xticks(df['year_month'][::4].dt.strftime('%Y-%m'), rotation=45)
 plt.grid(True)
 plt.tight_layout()
 plt.show()
 
 plt.figure(figsize=(12, 6))
-sns.lineplot(x='month_name', y='Payment from AESO (Thousands $)', data=df, marker='x')
+sns.lineplot(x='year_month', y='Payment from AESO (Thousands $)', data=df, marker='o')
 plt.title("Monthly Payment from AESO (Thousands $)", fontsize=16)
 plt.ylabel("Payment from AESO (Thousands $)", fontsize=14)
 plt.xlabel("Month", fontsize=14)
-plt.xticks(rotation=45)
+plt.xticks(df['year_month'][::4].dt.strftime('%Y-%m'), rotation=45)
 plt.grid(True)
 plt.tight_layout()
 plt.show()
